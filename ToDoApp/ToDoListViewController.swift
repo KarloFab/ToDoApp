3c563838cc3a70a90f29cd0ef763644da2c12b2e
KarloFab <fabijanickarlo1@gmail.com>
2021-12-20 22:32:25 +0100

    Added methods for populating TableView

--- ToDoApp/ToDoListViewController.swift
+++ ToDoApp/ToDoListViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
+    
+    let items = ["Swift Tutorial", "Node Tutorial"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
 
 }
 

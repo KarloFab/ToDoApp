c6292f46c1381719fd2f001abae35cc99abde9dd
KarloFab <fabijanickarlo1@gmail.com>
2021-12-28 16:39:31 +0100

    Added new model for item

---
+++ ToDoApp/Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  ToDoApp
+//
+//  Created by Karlo Fabijanić on 28.12.2021..
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
